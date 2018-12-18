88272d09280531aa02cbd508a1a9170aef00040a
liukai <liukai@jiduo365.cn>
2018-12-18 14:47:07 +0800

    Initial Commit

---
+++ AutoLayout/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AutoLayout
+//
+//  Created by liu kai on 2018/12/18.
+//  Copyright © 2018 liu kai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AutoLayout/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AutoLayout
+//
+//  Created by liu kai on 2018/12/18.
+//  Copyright © 2018 liu kai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
